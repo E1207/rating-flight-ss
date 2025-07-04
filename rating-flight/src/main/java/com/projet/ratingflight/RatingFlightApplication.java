94de56f719cf5f1a781ecd08bb350e359caced22
Emmanuel Gabriel Nzouawa Pondi <emmanuelpondi07@gmail.com>
2025-07-04 17:17:56 +0200

    First commit

---
+++ rating-flight/src/main/java/com/projet/ratingflight/RatingFlightApplication.java
@@ -0,0 +1,13 @@
+package com.projet.ratingflight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingFlightApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RatingFlightApplication.class, args);
+    }
+
+}
---
+++ rating-flight/src/test/java/com/projet/ratingflight/RatingFlightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projet.ratingflight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingFlightApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
